49d084c55323a2de645a5ab826e634fef7538c30
Per Mosegaard <permosegaard@googlemail.com>
2016-05-13 22:17:24 +0100

    Update startup.sh
    minor change

--- startup.sh
+++ startup.sh
@@ -2,6 +2,8 @@
 
 ip route change default via 172.17.42.254 && echo "nameserver 172.17.42.254" > /etc/resolv.conf
 
+ulimit -n 2048
+
 /root/steamcmd/steamcmd.sh +login anonymous +force_install_dir /server +app_update 376030 validate +quit
 
 settings_array=(
