25d4039a3812c5fae4261933ae716af7562fd84c
Per Mosegaard <permosegaard@googlemail.com>
2016-05-14 21:59:24 +0100

    Update startup.sh
    minor update

--- startup.sh
+++ startup.sh
@@ -4,7 +4,12 @@ ip route change default via 172.17.42.254
 
 ulimit -n 2048
 
-/root/steamcmd/steamcmd.sh +login anonymous +force_install_dir /server +app_update 376030 +quit
+if [ "$( find /server/ -type f | wc -l )" -lt "1" ]
+then
+  echo "copying seed across..."
+  time cp -Ra /seed/${CONTAINER_TYPE}/ /server/
+fi
+/root/steamcmd/steamcmd.sh +login anonymous +force_install_dir /server +app_update 376030 validate +quit
 
 settings_array=(
 	"SessionName=${CONTAINER_NAME}"
