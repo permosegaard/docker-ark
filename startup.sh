7eef58d677291563c558b41feacac18b20ff2ff0
Per Mosegaard <permosegaard@googlemail.com>
2016-05-16 16:14:47 +0100

    Update startup.sh
    minor update

--- startup.sh
+++ startup.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
 ip route change default via 172.17.42.254
+if [ -z "${STEAM_USER}" ]; then STEAM_CREDENTIALS="anonymous"; else STEAM_CREDENTIALS="${STEAM_USERNAME} ${STEAM_PASSWORD}"; fi
 
-ulimit -n 2048
+# start seed update section
+# shell in and rsync /server/* and /root/{Steam,steamcmd}/* to host /seed/$type/{game,steam,steamcmd}
+curl -s "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar -vzx -C "/root/steamcmd/"
+/root/steamcmd/steamcmd.sh +login $STEAM_CREDENTIALS +force_install_dir /server +app_update 376030 +quit
+apt-get update && apt-get install -y rsync openssh-client && echo && echo "update complete, pausing..." && read && exit
+# end seed update section
 
-if [ "$( find /server/ -type f | wc -l )" -lt "1" ]; then echo "copying seed across... this may take some time depending on the game size"; time cp -Rs /seed/${CONTAINER_TYPE}/ /server/; fi
-/root/steamcmd/steamcmd.sh +login anonymous +force_install_dir /server +app_update 376030 validate +quit
+if [ "$( find /server/ -type f | wc -l )" -lt "1" ]
+then
+  echo "copying seed across... this may take some time depending on the game size"
+  rm -Rf /server/* /root/steamcmd/*
+  cp -Rf /seed/${CONTAINER_TYPE}/game/* /server/ # change to hard/soft links
+  cp -Rf /seed/${CONTAINER_TYPE}/steamcmd/* /root/steamcmd/ # change to hard/soft links
+  cp -Rf /seed/${CONTAINER_TYPE}/steam/* /root/Steam/ # change to hard/soft links
+fi
+
+root/steamcmd/steamcmd.sh +login $STEAM_CREDENTIALS +force_install_dir /server +app_update 376030 validate +quit
 
 settings_array=(
 	"SessionName=${CONTAINER_NAME}"
@@ -16,4 +30,4 @@ settings_array=(
 )
 settings_string="$( printf "?%s" "${settings_array[@]}" )"
 
-cd /server/ && ShooterGame/Binaries/Linux/ShooterGameServer TheIsland?${settings_string} -server -log
+ulimit -n 2048 && cd /server/ && ShooterGame/Binaries/Linux/ShooterGameServer TheIsland?${settings_string} -server -log
